5304362c19b290ae7fccc6d84fa848df333c9338
xyl <447969163@qq.com>
2020-09-13 23:56:13 +0800

    修改了一处错误

--- api/app.js
+++ api/app.js
@@ -42,8 +42,7 @@ app.use((req,res,next)=>{
             res.send({"code":403,"msg":"token超时"})
         } else if (result.name == 'JsonWebTokenError') { // 如果返回结果是JsonWebTokenError，则说明token不对
             res.send({"code":403,"msg":"token错误"})
-        } else { // 如果正确解析了数据对象，将数据对象赋值给data，继续执行
-            req.data = result
+        } else { // 如果正确解析了数据对象，就继续执行
             next()
         }
     } else {
